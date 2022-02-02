da812c3f9493284aa1870fd25af0f5077a0389fc
root <root@spectrum.localdomain>
2022-02-02 19:33:39 +0100

    cleaning some stuff

--- tests/windows.ts
+++ tests/windows.ts
@@ -6,7 +6,7 @@ import ProtocolRegistry from "../src/index.ts";
 console.log("Registering...");
 const res = await ProtocolRegistry.register({
 	protocol: "denoapp",
-	command: `C:\\Users\\zenze\\.deno\\bin\\deno.exe run -A --unstable ${path.join(Deno.cwd(), "/tests/denoapp.ts")} $_URL_`,
+	command: `C:\\Users\\username\\.deno\\bin\\deno.exe run -A --unstable ${path.join(Deno.cwd(), "/tests/denoapp.ts")} $_URL_`,
 	override: true,
 	terminal: true,
 	script: true,
